1f286b53100aacd79e7344e370cdd4323a82626c
Larry Ruckman <ruckman@slac.stanford.edu>
2021-05-21 10:40:39 -0700

    bug fix

--- software/setup_env_afs_slac.sh
+++ software/setup_env_afs_slac.sh
@@ -1,5 +1,6 @@
 # Setup environment
-source /afs/slac.stanford.edu/g/reseng/rogue/anaconda/rogue_pre-release.sh
+source /afs/slac.stanford.edu/g/reseng/rogue/anaconda/miniconda3/etc/profile.d/conda.sh
+conda activate rogue_v5.7.0
 
 # Setup python path
 export PYTHONPATH=${PWD}/../firmware/submodules/surf/python:${PWD}/../firmware/submodules/lsst-pwr-ctrl-core/python:${PYTHONPATH}
