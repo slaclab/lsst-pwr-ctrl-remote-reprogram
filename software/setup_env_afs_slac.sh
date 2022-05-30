ab39e268c80a4248e87fde04dad52e9077fcc1e3
Larry Ruckman <ruckman@slac.stanford.edu>
2022-05-29 19:06:47 -0700

    updating LICENSE.txt and rogue conda setup

--- software/setup_env_afs_slac.sh
+++ software/setup_env_afs_slac.sh
@@ -1,6 +1,6 @@
 # Setup environment
-source /afs/slac.stanford.edu/g/reseng/rogue/anaconda/miniconda3/etc/profile.d/conda.sh
-conda activate rogue_v5.7.0
+source /afs/slac.stanford.edu/g/reseng/vol31/anaconda/anaconda3/etc/profile.d/conda.sh
+conda activate rogue_v5.10.0
 
 # Setup python path
 export PYTHONPATH=${PWD}/../firmware/submodules/surf/python:${PWD}/../firmware/submodules/lsst-pwr-ctrl-core/python:${PYTHONPATH}
